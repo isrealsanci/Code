38d1273b424010e1fa4b2ea64e27934e72158b69
sanci <126186524+isrealmuto@users.noreply.github.com>
2025-05-13 17:09:32 +0700

    Update wagmi.ts

--- spin/src/wagmi.ts
+++ spin/src/wagmi.ts
@@ -5,7 +5,7 @@ import {metaMask} from "wagmi/connectors";
 
 export const config = createConfig({
   chains: [base, mainnet],
-  connectors: [ metaMask()],
+  connectors: [ farcasterFrame()],
   transports: {
     [base.id]: http(),
     [mainnet.id]: http(),
