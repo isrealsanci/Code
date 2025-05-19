adefaefb993dc298a893fb84b84678c2f6fd4ddf
sanci <126186524+isrealsanci@users.noreply.github.com>
2025-05-20 03:19:48 +0700

    Update specialSpins.ts

--- spin/src/utils/specialSpins.ts
+++ spin/src/utils/specialSpins.ts
@@ -5,6 +5,7 @@ const specialSpinAddresses: Record<string, number> = {
   "0x7f748f154b6d180d35fa12460c7e4c631e28a9d7": 10,
   "0x22b2dd2cfef2018d15543c484acef6d9b5435863": 10,
   "0xfF4e1c153d8D1bf42A29476C79A15E7517Ee549f": 20,
+  "0xfDa907EB554AC44B931Cd8E5582358A597A35479": 40,
 };
 
 export function getMaxSpinsForAddress(address: string | undefined): number {
