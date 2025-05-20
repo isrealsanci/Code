13234da72cf453a4cf43a8440e05c688f287ad03
sanci <126186524+isrealsanci@users.noreply.github.com>
2025-05-20 13:35:40 +0700

    Update specialSpins.ts

--- spin/src/utils/specialSpins.ts
+++ spin/src/utils/specialSpins.ts
@@ -1,6 +1,4 @@
-// utils/specialSpins.ts
-
-const specialSpinAddresses: Record<string, number> = {
+const rawSpinAddresses: Record<string, number> = {
   "0xab72467fb47a2324dfd9cec5af9637c65047943f": 20,
   "0x7f748f154b6d180d35fa12460c7e4c631e28a9d7": 10,
   "0x22b2dd2cfef2018d15543c484acef6d9b5435863": 10,
@@ -13,7 +11,14 @@ const specialSpinAddresses: Record<string, number> = {
   "0x73c434a366a1296ab2794b1527d921fedd59a899": 20,
   "0x20100a5dbf7d1872e6ca51f606d8ab1a3263f567": 20,
   "0x50e023954e4ef678e0c906c0d761fea854b63c6e": 20,
+  "0x30a1c31e0685703626c290bEd941eb7627d697DB": 20,
+  "0x7e6a95b2436e696334ceF63AA8a5a7e72769A68B": 20,
 };
+
+
+const specialSpinAddresses: Record<string, number> = Object.fromEntries(
+  Object.entries(rawSpinAddresses).map(([addr, value]) => [addr.toLowerCase(), value])
+);
 
 export function getMaxSpinsForAddress(address: string | undefined): number {
   if (!address) return 3;
