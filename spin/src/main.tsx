aa17f160b4926494e5456537f4a289c9bffb20c0
sanci mothe <nulstaco@gmail.com>
2025-05-24 20:15:33 +0000

    error analys

--- spin/src/main.tsx
+++ spin/src/main.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { WagmiProvider } from "wagmi";
