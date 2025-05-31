7efea7eabcbedde3212852da5d1cb450507b3f12
sanci <126186524+isrealsanci@users.noreply.github.com>
2025-05-31 14:11:55 +0700

    Update wagmi.ts

--- spin/src/wagmi.ts
+++ spin/src/wagmi.ts
@@ -1,7 +1,6 @@
 import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
 import { http, createConfig } from "wagmi";
 import { base, mainnet } from "wagmi/chains";
-import {metaMask} from "wagmi/connectors";
 
 export const config = createConfig({
   chains: [base, mainnet],
